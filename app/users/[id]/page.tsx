37b494649a5420cb57b83b5ca4d076162da5e18e
mtuor <manueldorian.tuor@uzh.ch>
2025-02-23 15:16:31 +0100

    resolving compatibility issues

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -2,3 +2,14 @@
 // each user has their own slug /[id] (/1, /2, /3, ...) and is displayed using this file
 // try to leverage the component library from antd by utilizing "Card" to display the individual user
 // import { Card } from "antd"; // similar to /app/users/page.tsx
+
+const UserPage = () => {
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <p>Showing details for user ID: </p>
+    </div>
+  );
+};
+
+export default UserPage;
