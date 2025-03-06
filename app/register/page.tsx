d8e4aee49ef426a8a35a04f8547ba31b65325802
manueltuor <91838413+manueltuor@users.noreply.github.com>
2025-03-06 18:15:07 +0100

    test

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,5 @@
 "use client"; // For components that need React hooks and browser APIs, SSR (server side rendering) has to be disabled. Read more here: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
 
-
 import { useRouter } from "next/navigation"; // use NextJS router for navigation
 import { useApi } from "@/hooks/useApi";
 import useLocalStorage from "@/hooks/useLocalStorage";
